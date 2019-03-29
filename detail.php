bf566de58b2d98f3882968d4c967ac8ef4ff987b
MuchNesha <41299486+MuchNesha@users.noreply.github.com>
2019-03-29 13:51:29 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -26,7 +26,8 @@ foreach($obj->results as $item){
 }
 
 $title = "Detail dan Lokasi : ".$titles;
-include_once "header.php"; ?>
+include_once "header.php"; ?> 
+
 
 <script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyC5Jrp9PtHe0WapppUzxbIpMDWMAcV3qE4"></script>
 
